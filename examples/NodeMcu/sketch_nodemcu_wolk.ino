e68c6b84fc177c58147d03fe93aab90b387bd70d
Sava Vranesevic <sava.vranesevic@wolkabout.com>
2017-11-30 10:57:53 +0100

    examples/NodeMcu: Updated server address

--- examples/NodeMcu/sketch_nodemcu_wolk.ino
+++ examples/NodeMcu/sketch_nodemcu_wolk.ino
@@ -4,7 +4,7 @@
 
 const char* ssid = "wifi_ssid";
 const char* password = "wifi_password";
-const char* mqtt_server = "integration.wolksense.com";
+const char* mqtt_server = "api-demo.wolkabout.com";
 
 const char *device_key = "device_key";
 const char *password_key = "password_key";
@@ -46,7 +46,7 @@ void setup() {
   setup_wifi();
 
   wolk_set_protocol(&wolk, PROTOCOL_TYPE_JSON);
-  
+
   wolk_connect(&wolk, &client, mqtt_server, portno, device_key, password_key);
 
   wolk_set_actuator_references (&wolk, 2, numeric_slider_reference, bool_switch_reference);
@@ -56,7 +56,7 @@ void setup() {
   wolk_publish_bool_actuator_status (&wolk,bool_switch_reference, true, ACTUATOR_STATUS_READY, 0);
 
   wolk_publish_single (&wolk, "TS", "Arduino", DATA_TYPE_STRING, 0);
-  
+
   wolk_publish_single (&wolk, "TN", "30", DATA_TYPE_NUMERIC, 0);
 
   wolk_publish_single (&wolk, "TB", "true", DATA_TYPE_BOOLEAN, 0);
@@ -70,7 +70,7 @@ void setup() {
   wolk_add_numeric_reading(&wolk, "TN", 100, 0);
 
   wolk_publish (&wolk);
-  
+
 }
 
 void loop() {
@@ -101,11 +101,8 @@ void loop() {
       {
         wolk_publish_bool_actuator_status (&wolk,bool_switch_reference, false, ACTUATOR_STATUS_READY, 0);
       }
-      
     }
-  } 
- 
+  }
+
   delay(1000);
-  
-  
 }
