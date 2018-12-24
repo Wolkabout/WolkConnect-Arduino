8d19bb1e55990ef5d28f9024a6f45ad1df6f892c
LazarBozic94 <lazar.bozic@wolkabout.com>
2018-12-24 16:36:56 +0100

    added version

--- src/WolkConn.h
+++ src/WolkConn.h
@@ -36,7 +36,9 @@
 extern "C" {
 #endif
 
-#define VERSION 20100 //2.1.0
+#define FW_VERSION_MAJOR 2 // number 0 -99
+#define FW_VERSION_MINOR 1 // number 0 -99
+#define FW_VERSION_PATCH 0 // number 0 -99
 
 /**
  * @brief Supported protocols, WolkConnect libararies currently support only PROTOCOL_JSON_SINGLE
