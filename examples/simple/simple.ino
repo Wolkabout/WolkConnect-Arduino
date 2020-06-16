d2122c7245b6753b9dfb491dd9f98ffb81414c6b
mladen-vukovic <56019749+mladen-vukovic@users.noreply.github.com>
2020-06-16 15:54:42 +0200

    Udate protocol

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -53,7 +53,7 @@ void setup() {
   setup_wifi();
 
   wolk_init(&wolk, NULL, NULL, NULL, NULL,
-            device_key, device_password, &client, hostname, portno, PROTOCOL_JSON_SINGLE, NULL, NULL);
+            device_key, device_password, &client, hostname, portno, PROTOCOL_WOLKABOUT, NULL, NULL);
 
   wolk_init_in_memory_persistence(&wolk, &outbound_messages, sizeof(outbound_messages), false);
 
